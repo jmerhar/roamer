53dafb1d8e1ca89871c9204d8f3d7a03c4f61bc3
Jure Merhar <dev@merhar.si>
2026-06-30 11:38:47 +0200

    chore: release v1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "si.merhar.roamer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
 
